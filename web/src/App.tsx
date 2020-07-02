4c5319759a92a664041c151190b483fac7a2835a
GledsonS831 <glegogle84@gmail.com>
2020-07-02 07:23:24 -0300

    :collision: feat(web): config api acess

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 function App() {
+  /* const [pokemon, pokemonState] = useState<string>(); */
+  
+  useEffect(() =>{
+    api.get('ditto').then((res) =>{
+      console.log(res.data)
+    })
+  }, [])
+
   return (
-    <h1>Hello pokestore</h1>
+    <h1>a</h1>
   );
 }
 
