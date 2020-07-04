7bd15b0deb3aacfdf982bd977c42183990c929a8
GledsonS831 <glegogle84@gmail.com>
2020-07-04 10:51:13 -0300

    :collision: feat(web): create component Search

---
+++ web/src/components/Search/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as St from './styles';
+
+const Search: React.FC = () =>{
+    return (
+        <St.Search></St.Search>
+    )
+}
+
+export default Search;
---
+++ web/src/components/Search/styles.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components'
+
+export const Search = styled.div`
+    width: 600px;
+    height: 40px;
+    display: flex;
+`
