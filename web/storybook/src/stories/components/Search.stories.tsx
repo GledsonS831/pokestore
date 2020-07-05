b6ee2cbed90db6348a3beeb678bd86ff82ff5bd5
GledsonS831 <glegogle84@gmail.com>
2020-07-05 03:31:54 -0300

    :hammer: chore(web): change configs Search component

---
+++ web/storybook/src/stories/components/Search.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Search from '../../../../src/components/Search';
+
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Search', module).add('Container', () => <Search typeInput="search" placeholderInput="Digite o pokemon que você procura..." />);
