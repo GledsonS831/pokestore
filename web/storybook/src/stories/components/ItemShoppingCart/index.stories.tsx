f2a78ce74861fc39456f477d9ce0e9b7d961af79
GledsonS831 <glegogle84@gmail.com>
2020-07-05 08:46:38 -0300

    :hammer: chore(web): add storybook cart

---
+++ web/storybook/src/stories/components/ItemShoppingCart/index.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import ItemShoppingCart from '../../../../../src/components/ItemShoppingCart';
+
+import { storiesOf } from '@storybook/react';
+
+storiesOf('ItemShoppingCart', module).add('Cart', () => <ItemShoppingCart />)
