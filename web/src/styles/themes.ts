196d7332d066666736684ab464bac8dafe522cfa
GledsonS831 <glegogle84@gmail.com>
2020-07-05 03:33:57 -0300

    :book: docs(web): update styles

--- web/src/styles/themes.ts
+++ web/src/styles/themes.ts
@@ -1,5 +1,12 @@
 export const global = {
     colors: {
-        main: '#2C1266'
+        main: '#2C1266',
+        secondary: '#87148f',
+        shadowPink: '#ff0557',
+        background: 'white'
+    },
+    fonts: {
+        ubuntu: '../assets/Fonts/UbuntuCondensed-Regular.ttf',
+        modal: '../assets/Fonts/Modak-Regular.ttf'
     }
 }
