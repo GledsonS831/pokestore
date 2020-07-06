ec5104c0ae889a98bd923f82b9e666a1536e17b6
GledsonS831 <glegogle84@gmail.com>
2020-07-06 10:47:01 -0300

    :pencil2: refactor(web): add style App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import api from './services/api';
-
+import './App.css'
 import Routes from './routes/routes';
 
-function App() {  
+function App() {
   return (
     <Routes />
   );
