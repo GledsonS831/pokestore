17bf8afaf7569e0f3f62d6a3ab153a26bc92ebc0
GledsonS831 <glegogle84@gmail.com>
2020-07-06 10:48:26 -0300

    :pencil2: refactor(web): add new component in Home

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-
 import Header from '../../components/Header';
+import OptionSection from '../../components/OptionSection';
+
 const Home = () => {
     return (
         <>
             <Header />
+            <OptionSection />
         </>
     )
-
 }
 
 export default Home;
