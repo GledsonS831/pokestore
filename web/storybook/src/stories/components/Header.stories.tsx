73bf299a698f2f4504db7121b4ce02189bc3bea4
GledsonS831 <glegogle84@gmail.com>
2020-07-08 02:47:21 -0300

    :hammer: chore(web): update storybook

---
+++ web/storybook/src/stories/components/Header.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import Search from '../../../../src/components/Header/Search'
+
+storiesOf('Header', module).add('Search', () => <Search />)
