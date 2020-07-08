3f3def37cf86ee7223c1b19c9c9438da997daabd
GledsonS831 <glegogle84@gmail.com>
2020-07-08 02:46:15 -0300

    :hammer: chore(web): add changes on Home

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Header from '../../components/Header';
 import OptionSection from '../../components/OptionSection';
 
+import ItemShopping from '../../components/ShoppingCatalog/ShoppingList/ItemShoppingBuy';
+
 const Home = () => {
     return (
         <>
             <Header />
             <OptionSection />
+            <ItemShopping />
         </>
     )
 }
