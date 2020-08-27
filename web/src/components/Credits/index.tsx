4ecbcc8ceac880c7885fc17f6af93c8613d4673c
GledsonS831 <glegogle84@gmail.com>
2020-08-27 00:49:46 -0300

    :hammer: chore(web): add restart function in thanks page

--- web/src/components/Credits/index.tsx
+++ web/src/components/Credits/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { usePokemon } from "../../hooks/AppProvider";
+import { useCart } from "../../hooks/CartProvider";
 import * as St from "./styles";
 
 const Credits: React.FC = () => {
+  const { setProductList } = useCart();
+  const { setItemsCount } = usePokemon();
   return (
     <St.Container>
       <St.MessageContainer>
@@ -14,8 +18,17 @@ const Credits: React.FC = () => {
             Com essa aplicaçao foi possível realizar uma aplicação com o React.
           </St.Message>
           <St.Message>
-            Clique <St.BackLink to="/">aqui</St.BackLink> para reiniciar a
-            compra.
+            Clique{" "}
+            <St.BackLink
+              to="/"
+              onClick={() => {
+                setProductList([]);
+                setItemsCount(0);
+              }}
+            >
+              aqui
+            </St.BackLink>{" "}
+            para reiniciar a compra.
           </St.Message>
         </St.MessageMainContainer>
       </St.MessageContainer>
